f29eae3a96272f5a06ca84ea377def0d2eb85a08
iiqcov <iiqcov@gmail.com>
2024-04-20 22:35:55 +0900

    feat : oauth2 관련 설정 추가

---
+++ src/main/java/com/security/global/common/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.security.global.common;
+
+public class SecurityConstants {
+    public static final String KAKAO_JWK_SET_URL = "https://kauth.kakao.com/.well-known/jwks.json";
+    public static final String KAKAO_ISSUER = "https://kauth.kakao.com";
+}
